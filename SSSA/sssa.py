d519136a2e0c064d12fd3bc30bdedd16d5af6f43
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-10-29 10:19:57 -0500

    Code changes; update to new prime

--- SSSA/sssa.py
+++ SSSA/sssa.py
@@ -1,4 +1,9 @@
+from utils import utils
+
 class sssa:
+    prime = 115792089237316195423570985008687907853269984665640564039457584007913129639747
+    util = utils()
+
     def create(minimum, shares, raw):
         return [""]
 
--- SSSA/utils.py
+++ SSSA/utils.py
@@ -1,3 +1,16 @@
+from random import SystemRandom
+
 class utils:
-    def random():
-        return 300
+    prime = 0
+
+    def __init__(self, prime=115792089237316195423570985008687907853269984665640564039457584007913129639747):
+        self.prime = prime
+
+    def random(self):
+        return SystemRandom().randrange(self.prime)
+
+    def split_ints(self, secret):
+        result = []
+
+        for i in range(len(secret)/32 + 1):
+            text =
