d5cb5744b4e45d2531d2824095d4d7102e14d752
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-11-07 21:43:29 -0600

    Added cross-language tests

--- sssa_tests.py
+++ sssa_tests.py
@@ -12,5 +12,11 @@ class TestStringMethods(unittest.TestCase):
         for index,value in enumerate(values):
             self.assertEqual(sss.combine(sss.create(minimum[index], shares[index], value)), value)
 
+    def test_library_combine(self):
+        sss = sssa()
+        shares = ["U1k9koNN67-og3ZY3Mmikeyj4gEFwK4HXDSglM8i_xc=yA3eU4_XYcJP0ijD63Tvqu1gklhBV32tu8cHPZXP-bk=", "O7c_iMBaGmQQE_uU0XRCPQwhfLBdlc6jseTzK_qN-1s=ICDGdloemG50X5GxteWWVZD3EGuxXST4UfZcek_teng=", "8qzYpjk7lmB7cRkOl6-7srVTKNYHuqUO2WO31Y0j1Tw=-g6srNoWkZTBqrKA2cMCA-6jxZiZv25rvbrCUWVHb5g=", "wGXxa_7FPFSVqdo26VKdgFxqVVWXNfwSDQyFmCh2e5w=8bTrIEs0e5FeiaXcIBaGwtGFxeyNtCG4R883tS3MsZ0=", "j8-Y4_7CJvL8aHxc8WMMhP_K2TEsOkxIHb7hBcwIBOo=T5-EOvAlzGMogdPawv3oK88rrygYFza3KSki2q8WEgs="]
+
+        self.assertEqual(sss.combine(shares), "test-pass")
+
 if __name__ == '__main__':
     unittest.main()
